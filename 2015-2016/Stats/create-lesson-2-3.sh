1bca94bb44f2ec7ea22763700ce4d5a80de5c637
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2015-08-06 12:09:03 +0200

    creating all files

--- 2015-2016/Stats/create-lesson-2-3.sh
+++ 2015-2016/Stats/create-lesson-2-3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-runKnitr.sh Lesson-2.Rnw
+runKnitr-2.sh Lesson-2.Rnw 1
 rm Lesson-2-files.zip
 mkdir Lesson-2-files
 cp Lesson-2.pdf P53.txt MYC.txt BRCA2.txt ./Lesson-2-files/.
 zip -r Lesson-2-files.zip Lesson-2-files
 
-runKnitr.sh Lesson-3.Rnw
+runKnitr-2.sh Lesson-3.Rnw 1
 rm Lesson-3-files.zip
 mkdir Lesson-3-files
 cp Lesson-3.pdf MIT.txt Cholesterol.txt AnAge_birds_reptiles.txt CystFibr2.txt ./Lesson-3-files/.
