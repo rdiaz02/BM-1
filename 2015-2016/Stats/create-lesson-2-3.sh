a841a5e010c07800c891cef9b3db4ce3f97e94f2
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2015-08-08 18:47:33 +0200

    minor to making all files

--- 2015-2016/Stats/create-lesson-2-3.sh
+++ 2015-2016/Stats/create-lesson-2-3.sh
@@ -2,7 +2,8 @@
 runKnitr-2.sh Lesson-2.Rnw 1
 rm Lesson-2-files.zip
 mkdir Lesson-2-files
-cp Lesson-2.pdf P53.txt MYC.txt BRCA2.txt ./Lesson-2-files/.
+rm Lesson-2-files/*
+cp P53.txt MYC.txt BRCA2.txt ./Lesson-2-files/.
 zip -r Lesson-2-files.zip Lesson-2-files
 
 runKnitr-2.sh Lesson-3.Rnw 1
