2a56360e8fd1efb07a8bfde5df9f6d45e2999575
ramon diaz-uriarte (at Podarcis) <rdiaz02@gmail.com>
2016-09-13 16:51:17 +0200

    updated anova diagnostics

--- 2016-2017/Stats/example-anova-diagnostics.R
+++ 2016-2017/Stats/example-anova-diagnostics.R
@@ -55,6 +55,7 @@ myAdditive2b <- lm(y ~ sex + drug, data = y.data2)
 
 ## ## diagnostics, all of them except 6th
 ## we actually have a large Cook's distance
+dev.off()
 par(mfrow = c(2, 3))
 plot(myAdditive2b, which = 1:5)
 
@@ -79,27 +80,58 @@ plot(myAdditive3, which = 1:5) ## see Cook's
 
 
 
+########################################
+###                    #################
+### For class, summary
+###                    #################
+########################################
+
+
+## Diagnostics can suggest missing interaction
+
+dev.off()
+par(mfrow = c(2, 3))
+plot(myAdditive2, which = c(1:5)) ## look at first plot
+
+dev.off()
+par(mfrow = c(2, 3))
+plot(myInteract2, which = c(1:5))
+
+
+
+## Where is Cook's distance and a large outlier/large Cook.
+## But the problem here is not a missing interaction
+dev.off()
+par(mfrow = c(2, 3))
+plot(myAdditive2b, which = 1:5)
+
+
+## Same model, offending observation removed We are shown the Cook's plot
+## by default (and no longer any concerns in first plot)
+dev.off()
+par(mfrow = c(2, 3))
+plot(lm(y ~ sex + drug, data = y.data2[-1, ]), which = 1:5)
+
+
+
+## For the sake of it, if we had lack of balance to being with, we are
+## always shown Cook's
+dev.off()
+par(mfrow = c(2, 3))
+plot(myAdditive3, which = 1:5) ## see Cook's
+
+
+
+
+###########################################################################
+
+
+
 
 
 ## some details, from plot.lm
 lm.influence(myAdditive2b, do.coef = FALSE)$hat ## constant
 lm.influence(myAdditive3, do.coef = FALSE)$hat ##nope
 diff(range(hii))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
